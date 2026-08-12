19c7d31e06d2ce9674f31af94d6985dcf082d2bf
Foysal Hossien <foysalhossien77@gmail.com>
2026-08-13 04:56:58 +0600

    feat: select modal elements for DOM manipulation

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModalBtn = document.querySelector('.close-modal');
+const opensModalBtn = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < opensModalBtn.length; i++) {
+  opensModalBtn[i].textContent;
+}
